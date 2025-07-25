80ad98a1c026a5bfe360faacf064eec1690fdd75
Komi Golov <komi.golov@jetbrains.com>
2025-07-25 07:28:29 +0200

    Play around with linked list example

--- formver.compiler-plugin/testData/diagnostics/verification/uniqueness/linked_list.kt
+++ formver.compiler-plugin/testData/diagnostics/verification/uniqueness/linked_list.kt
@@ -1,11 +1,23 @@
 // RENDER_PREDICATES
 
-import org.jetbrains.kotlin.formver.plugin.NeverConvert
-import org.jetbrains.kotlin.formver.plugin.NeverVerify
+import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
 import org.jetbrains.kotlin.formver.plugin.Unique
+import org.jetbrains.kotlin.formver.plugin.verify
 
-class Link(@Unique val next: Link?, @Unique var data: Int)
+class Link(var data: Int, @Unique val next: Link?)
 
+@AlwaysVerify
 fun <!VIPER_TEXT!>getVal<!>(@Unique l: Link): Int? {
     return l.next?.next?.data
+}
+
+fun f(a: Any) { ... }
+
+@AlwaysVerify
+fun <!VIPER_TEXT!>test<!>() {
+    val l = Link(5, Link(3, null))
+    val expr1 = l.data == 5
+    verify(expr1)
+    val expr2 = l.next?.data == 3
+    verify(expr2)
 }
