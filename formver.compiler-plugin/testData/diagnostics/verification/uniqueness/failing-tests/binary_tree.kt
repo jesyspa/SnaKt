73ecf7e1ce15145dc0dd80da524c20c6c12ab154
Sai Divvela <saidivvela2005@gmail.com>
2025-08-18 12:08:56 -0500

    refactored uniqueness tests to mark that they are failing

--- formver.compiler-plugin/testData/diagnostics/verification/uniqueness/failing-tests/binary_tree.kt
+++ formver.compiler-plugin/testData/diagnostics/verification/uniqueness/failing-tests/binary_tree.kt
@@ -1,6 +1,6 @@
 // RENDER_PREDICATES
 
-import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
+import org.jetbrains.kotlin.formver.plugin.NeverVerify
 import org.jetbrains.kotlin.formver.plugin.Unique
 import org.jetbrains.kotlin.formver.plugin.verify
 
@@ -11,11 +11,11 @@ fun <!VIPER_TEXT!>get_left_val<!>(@Unique n: Node): Int? {
 }
 
 // these expressions should all verify - they currently do not for what i think is the same reason as the linked list
-@AlwaysVerify
+@NeverVerify
 fun <!VIPER_TEXT!>test<!>() {
     val n = Node(5, Node(4, null, null), Node(3, Node(2, null, null), Node(1, null, null)))
     val expr1 = n.data == 5
-    verify(expr1)
+    verify(<!VIPER_VERIFICATION_ERROR!>expr1)
     val expr2 = n.left?.data == 4
-    verify(expr2)
+    verify(<!VIPER_VERIFICATION_ERROR!>expr2)
 }
--- formver.compiler-plugin/testData/diagnostics/verification/uniqueness/failing-tests/linked_list.kt
+++ formver.compiler-plugin/testData/diagnostics/verification/uniqueness/failing-tests/linked_list.kt
@@ -1,22 +1,22 @@
 // RENDER_PREDICATES
 
-import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
+import org.jetbrains.kotlin.formver.plugin.NeverVerify
 import org.jetbrains.kotlin.formver.plugin.Unique
 import org.jetbrains.kotlin.formver.plugin.verify
 
 class Link(var data: Int, @Unique val next: Link?)
 
-@AlwaysVerify
+@NeverVerify
 fun <!VIPER_TEXT!>getVal<!>(@Unique l: Link): Int? {
     return l.next?.data
 }
 
-
-@AlwaysVerify
+// these expressions should also all verify, but currently do not due to lack of uniqueness information
+@NeverVerify
 fun <!VIPER_TEXT!>test<!>() {
     val l = Link(5, Link(3, null))
     val expr1 = l.data == 5
-    verify(expr1)
+    verify(<!VIPER_VERIFICATION_ERROR!>expr1)
     val expr2 = l.next?.data == 3
-    verify(expr2)
+    verify(<!VIPER_VERIFICATION_ERROR!>expr2)
 }
