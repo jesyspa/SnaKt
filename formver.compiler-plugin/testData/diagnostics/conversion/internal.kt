e89a131650325418f4c15746a72c5a541c9a40c6
Grigorii Braulov <grigorii.braulov@jetbrains.com>
2025-08-22 05:38:37 +0300

    fix internal test

--- formver.compiler-plugin/testData/diagnostics/conversion/internal.kt
+++ formver.compiler-plugin/testData/diagnostics/conversion/internal.kt
@@ -1,25 +1,26 @@
 // NEVER_VALIDATE
 
 // Test function literals
-fun <!VIPER_TEXT!>testFunctionLiteral<!>() {
-    val f = { x: Int -> x + 1 } // Anonymous function
+fun testFunctionLiteral() {
+    val f = <!VIPER_VERIFICATION_ERROR!>{ x: Int -> x + 1 }<!> // Anonymous function
 }
 // Test Double literals
-fun <!VIPER_TEXT!>testDoubleLiteral<!>() {
-    val x = 1.0  // Double литерал
+fun testDoubleLiteral() {
+    val x = <!VIPER_VERIFICATION_ERROR!>1.0<!>  // Double литерал
 }
 
+/*
 // Test class-related errors
 class TestClass
-fun <!VIPER_TEXT!>testClassUsages<!>() {
+fun testClassUsages() {
     TestClass      // ResolvedQualifier
     TestClass::class  // Class literal
-}
+}*/
 
 // Test when expression
 fun <!VIPER_TEXT!>testWhen<!>(x: String) {
     when (x) {
-        1 -> println("one")
+        <!INCOMPATIBLE_TYPES!>1<!> -> println("one")
         else -> println("other")
     }
 }
