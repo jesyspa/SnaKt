3364aadf6d1151cc517d7588b0b711b9ec4ba4ce
AlexFalter <baumlp61@gmail.com>
2025-12-29 11:37:34 +0100

    PR adjustments

--- formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/linearization/SsaConverter.kt
+++ formver.compiler-plugin/core/src/org/jetbrains/kotlin/formver/core/linearization/SsaConverter.kt
@@ -11,7 +11,7 @@ class SsaConverter(
     val source: KtSourceElement? = null,
     val variableIndex: MutableMap<SymbolicName, Int> = mutableMapOf()
 ) {
-    private val assignments: MutableList<Triple<Declaration.LocalVarDecl, Int, Exp>> = mutableListOf()
+    private val assignments: MutableList<SsaAssignment> = mutableListOf()
     private var body: Exp? = null
 
     fun asExp(): Exp {
@@ -19,22 +19,32 @@ class SsaConverter(
             source,
             "Empty body cannot be converted into let chain"
         )
-        return assignments.foldRight(body as Exp) { (decl, ssaIdx, expr), acc ->
+        val bodyExp = body!!
+        return assignments.foldRight(bodyExp) { (decl, ssaIdx, expr), acc ->
             Exp.LetBinding(decl.copy(name = SsaVariableName(decl.name, ssaIdx)), expr, acc)
         }
     }
 
     fun addAssignment(decl: Declaration.LocalVarDecl, varExp: Exp) {
         variableIndex[decl.name] = variableIndex[decl.name]?.plus(1) ?: 0
-        assignments.add(Triple(decl, variableIndex[decl.name] as Int, varExp))
+        assignments.add(SsaAssignment(decl, variableIndex[decl.name] as Int, varExp))
     }
 
     fun addBody(body: Exp) {
         this.body = body
     }
 
+    /**
+     * Resolves a base variable name to the latest SSA variable name used in this or previous converters.
+     * The name is resolved as follows:
+     * 1. Check and use the name in the scope of this converter
+     * 2. If 1. fails, check and use the name in the scope of previous converters
+     * 3. If 2. fails use the base name (that is implicitly assume a function parameter is being resolved here)
+     * @return the resolved variable name of the provided name
+     */
     fun resolveVariableName(name: SymbolicName): SymbolicName =
-    // Fall back to parameter if no assignment in map is found
         // TODO: Fall back to global value numbering before falling back to parameter
-        assignments.lastOrNull { it.first.name == name }?.let { SsaVariableName(name, it.second) } ?: name
+        assignments.lastOrNull { it.declaration.name == name }?.let { SsaVariableName(name, it.ssaIdx) } ?: name
 }
+
+data class SsaAssignment(val declaration: Declaration.LocalVarDecl, val ssaIdx: Int, val exp: Exp)
