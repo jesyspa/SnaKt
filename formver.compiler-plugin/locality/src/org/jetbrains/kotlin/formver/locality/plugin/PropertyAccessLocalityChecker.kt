d0fa6c21fd2d7d6f2b6d770cf124f70b6a05aaca
marco <paganma@usi.ch>
2026-06-26 20:18:49 +0200

    Simplify PropertyAccessLocalityChecker

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/PropertyAccessLocalityChecker.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/PropertyAccessLocalityChecker.kt
@@ -7,114 +7,40 @@ package org.jetbrains.kotlin.formver.locality.plugin
 
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
-import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirPropertyAccessExpressionChecker
-import org.jetbrains.kotlin.fir.declarations.FirAnonymousFunction
 import org.jetbrains.kotlin.fir.declarations.FirFunction
-import org.jetbrains.kotlin.fir.declarations.FirProperty
-import org.jetbrains.kotlin.fir.expressions.FirCall
 import org.jetbrains.kotlin.fir.expressions.FirPropertyAccessExpression
-import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccessExpression
-import org.jetbrains.kotlin.fir.expressions.toResolvedCallableSymbol
 import org.jetbrains.kotlin.fir.references.symbol
-import org.jetbrains.kotlin.fir.resolve.dfa.controlFlowGraph
-import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirErrorFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirLocalPropertySymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirReceiverParameterSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirValueParameterSymbol
 
-context(context: CheckerContext)
-private fun FirFunctionSymbol<*>.declares(symbol: FirBasedSymbol<*>): Boolean =
-    when (symbol) {
-        is FirReceiverParameterSymbol ->
-            symbol.containingDeclarationSymbol == this
-        is FirValueParameterSymbol ->
-            symbol.containingDeclarationSymbol == this
-        is FirLocalPropertySymbol -> {
-            val graph = resolvedControlFlowGraphReference?.controlFlowGraph
-                ?: return false
+private fun CheckerContext.collectLocalDeclarations(): Sequence<FirFunctionSymbol<*>> {
+    val parentElements = containingElements.asReversed()
 
-            symbol in graph.resolveLocalPropertySymbols()
-        }
-        else -> false
-    }
+    return sequence {
+        for (parentElement in parentElements) {
+            if (parentElement is FirFunction) {
+                val functionSymbol = parentElement.symbol
 
-context(context: CheckerContext)
-private fun FirAnonymousFunction.lookupBoundLocality(parentElements: List<FirElement>): Locality {
-    for (parentElement in parentElements) {
-        when (parentElement) {
-            is FirProperty -> {
-                val initializer = parentElement.initializer ?: continue
+                yield(functionSymbol)
 
-                if (initializer.resolveLambdas().contains(this)) {
-                    return parentElement.symbol.resolveLocality()
-                }
+                if (functionSymbol.resolveLocality() != Locality.Local) break
             }
-            is FirCall -> {
-                val argumentLocalities = CallArgumentLocalitiesMapper.mapArgumentTypeFactsOf(parentElement)
-
-                for ((expression, locality) in argumentLocalities) {
-                    if (this in expression.resolveLambdas()) {
-                        return locality
-                    }
-                }
-
-                if (parentElement is FirQualifiedAccessExpression) {
-                    // NOTE: As of now, only extension receivers can be specified as local. It is not possible to
-                    // specify other receiver kinds.
-                    val extensionReceiver = parentElement.extensionReceiver
-
-                    if (extensionReceiver != null && extensionReceiver.resolveLambdas().contains(this)) {
-                        return parentElement.toResolvedCallableSymbol()
-                            ?.receiverParameterSymbol?.resolveLocality() ?: Locality.Global
-                    }
-                }
-            }
-            is FirFunction -> return Locality.Global
-            else -> continue
         }
     }
-
-    return Locality.Global
 }
-
-private val CheckerContext.localDeclarations: Sequence<FirFunctionSymbol<*>>
-    get() {
-        val parentElements = containingElements.asReversed()
-
-        return sequence {
-            for ((parentIndex, parentElement) in parentElements.withIndex()) {
-                if (parentElement !is FirFunction) continue
-
-                yield(parentElement.symbol)
-
-                if (parentElement is FirAnonymousFunction) {
-                    val currentParentElements = parentElements.subList(parentIndex + 1, parentElements.size)
-                    val boundLocality = parentElement.lookupBoundLocality(currentParentElements)
-
-                    if (boundLocality != Locality.Local) break
-                }
-            }
-        }
-    }
-
-private fun CheckerContext.declaresInLocalScope(symbol: FirBasedSymbol<*>): Boolean =
-    localDeclarations.any { declarationSymbol -> declarationSymbol.declares(symbol) }
 
 object PropertyAccessLocalityChecker : FirPropertyAccessExpressionChecker(MppCheckerKind.Common) {
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(expression: FirPropertyAccessExpression) {
-        val accessSymbol = expression.calleeReference.symbol ?: return
-
         if (expression.resolveLocality() == Locality.Global) return
 
-        val localDeclarations = context.localDeclarations
+        val symbol = expression.calleeReference.symbol ?: return
+        val localDeclarations = context.collectLocalDeclarations()
 
-        if (context.declaresInLocalScope(accessSymbol)) return
+        if (localDeclarations.any { declaration -> declaration.declares(symbol) }) return
 
         reporter.reportOn(
             expression.source,
