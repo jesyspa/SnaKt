df8e26c10f187c7879b4060e89b691a731b94317
marco <paganma@usi.ch>
2026-06-26 19:58:58 +0200

    Delete ExpressionLambdasResolver

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/LocalityExtensionRegistrar.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/LocalityExtensionRegistrar.kt
@@ -26,7 +26,6 @@ class LocalityExtensionRegistrar(
         registerDiagnosticContainers(LocalityErrors)
         registerDiagnosticContainers(LocalityContractErrors)
         +LocalityAttributeExtension.getFactory(localityAnnotationId)
-        +ExpressionLambdasResolver.getFactory()
         +ExpressionLocalityContractResolver.getFactory()
         +ExpressionLocalityResolver.getFactory()
         +GraphLocalPropertySymbolsResolver.getFactory()
