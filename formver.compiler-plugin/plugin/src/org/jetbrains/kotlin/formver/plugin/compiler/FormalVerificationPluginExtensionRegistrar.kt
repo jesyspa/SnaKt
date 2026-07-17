cd3b5d841ab21cdcb3475c44ac074c2d1ce5e2c5
marco <paganma@usi.ch>
2026-07-17 12:17:42 +0200

    Register uniqueness and locality session components in verification plugin

--- formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/FormalVerificationPluginExtensionRegistrar.kt
+++ formver.compiler-plugin/plugin/src/org/jetbrains/kotlin/formver/plugin/compiler/FormalVerificationPluginExtensionRegistrar.kt
@@ -8,12 +8,39 @@ package org.jetbrains.kotlin.formver.plugin.compiler
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.formver.common.PluginConfiguration
 import org.jetbrains.kotlin.formver.core.diagnostics.ConversionErrors
+import org.jetbrains.kotlin.formver.locality.contract.plugin.ExpressionLocalityContractResolver
+import org.jetbrains.kotlin.formver.locality.contract.plugin.LocalityContractErrors
+import org.jetbrains.kotlin.formver.locality.plugin.ExpressionLocalityResolver
+import org.jetbrains.kotlin.formver.locality.plugin.GraphCapturedSymbolsResolver
+import org.jetbrains.kotlin.formver.locality.plugin.GraphDeclaredSymbolsResolver
+import org.jetbrains.kotlin.formver.locality.plugin.GraphScopeLocalityResolver
+import org.jetbrains.kotlin.formver.locality.plugin.LocalityAttributeExtension
+import org.jetbrains.kotlin.formver.locality.plugin.LocalityErrors
+import org.jetbrains.kotlin.formver.uniqueness.plugin.ExpressionAccessStateResolver
+import org.jetbrains.kotlin.formver.uniqueness.plugin.ExpressionUniquenessResolver
+import org.jetbrains.kotlin.formver.uniqueness.plugin.GraphUniquenessStatesResolver
+import org.jetbrains.kotlin.formver.uniqueness.plugin.UniquenessAttributeExtension
+import org.jetbrains.kotlin.formver.uniqueness.plugin.UniquenessErrors
 
 class FormalVerificationPluginExtensionRegistrar(private val config: PluginConfiguration) : FirExtensionRegistrar() {
     override fun ExtensionRegistrarContext.configurePlugin() {
         registerDiagnosticContainers(PluginErrors)
         registerDiagnosticContainers(VerificationErrors)
         registerDiagnosticContainers(ConversionErrors)
+        registerDiagnosticContainers(UniquenessErrors)
+        registerDiagnosticContainers(LocalityErrors)
+        registerDiagnosticContainers(LocalityContractErrors)
+
+        +ExpressionAccessStateResolver.getFactory()
+        +ExpressionUniquenessResolver.getFactory()
+        +GraphUniquenessStatesResolver.getFactory()
+        +UniquenessAttributeExtension.getFactory()
+
+        +ExpressionLocalityContractResolver.getFactory()
+        +ExpressionLocalityResolver.getFactory()
+        +GraphLocalPropertySymbolsResolver.getFactory()
+        +LocalityAttributeExtension.getFactory()
+
         +PluginAdditionalCheckers.getFactory(config)
     }
 }
