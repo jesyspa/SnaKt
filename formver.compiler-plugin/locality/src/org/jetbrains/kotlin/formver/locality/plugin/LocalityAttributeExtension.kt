d43061e9c7b026f0caed8d41e7508ff2fac1ddc4
marco <paganma@usi.ch>
2026-07-17 12:14:19 +0200

    Add default annotation in getFactory constructor

--- formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/LocalityAttributeExtension.kt
+++ formver.compiler-plugin/locality/src/org/jetbrains/kotlin/formver/locality/plugin/LocalityAttributeExtension.kt
@@ -11,13 +11,21 @@ import org.jetbrains.kotlin.fir.extensions.FirTypeAttributeExtension
 import org.jetbrains.kotlin.fir.expressions.FirAnnotation
 import org.jetbrains.kotlin.fir.types.ConeAttribute
 import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+
+val defaultLocalityAnnotationId =
+    ClassId(
+        FqName("org.jetbrains.kotlin.formver.plugin"),
+        Name.identifier("Borrowed")
+    )
 
 class LocalityAttributeExtension(
     session: FirSession,
     private val annotationId: ClassId
 ) : FirTypeAttributeExtension(session) {
     companion object {
-        fun getFactory(annotationId: ClassId): Factory {
+        fun getFactory(annotationId: ClassId = defaultLocalityAnnotationId): Factory {
             return Factory { session -> LocalityAttributeExtension(session, annotationId) }
         }
     }
